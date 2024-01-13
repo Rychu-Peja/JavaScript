10a713b056e6f1752e482114ac9268d1e89285f4
Fryderyk Szmidt <f.szmidt.995@studms.ug.edu.pl>
2024-01-13 18:11:32 +0100

    DZIAŁA

--- event-planner/src/app.js
+++ event-planner/src/app.js
@@ -4,14 +4,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const eventRoutes = require('./routes/eventRoutes');
-const filePath = 'C:\\Users\\jedzi\\Desktop\\JS-zaliczenie\\event-planner\\public\\index.html';
+const filePath = 'C:\\Users\\jedzi\\Desktop\\JS-zaliczenie\\event-planner\\views\\index.ejs';
+const eventController = require('./controllers/eventController');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(express.static('public', { extensions: ['html', 'css', 'js'] }));
-app.use(express.static('src'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, '../views'));  // Poprawiona ścieżka do widoków
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Połączenie z MongoDB
 mongoose.connect('mongodb://localhost:27017/event-planner', {})
@@ -20,19 +23,14 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // Trasy
-app.use('/events', eventRoutes);
+//app.use('/events', eventRoutes);
+app.post('/events', eventController.createEvent);
 
 // Obsługa ścieżki głównej
 app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  readHTMLFile(filePath, (err, html) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.send(html);
-    }
-  });
+  res.render('index', { eventName: '', eventDate: '', eventTime: '' }); // Dodaj puste wartości na początku
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
