791555d6475234351b9aa9ee0857f92d019a5886
Nikodem <nikodemus.mat@gmail.com>
2024-01-13 12:54:32 +0100

    frontend

--- event-planner/src/app.js
+++ event-planner/src/app.js
@@ -4,19 +4,17 @@ const fs = require('fs');
 function readHTMLFile(filePath, callback) {
   fs.readFile(filePath, { encoding: 'utf-8' }, (err, html) => {
     if (err) {
-      throw err;
+      callback(err);
     } else {
       callback(null, html);
     }
   });
 }
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const eventRoutes = require('./routes/eventRoutes');
-const filePath = 'C:\\Users\\jedzi\\Desktop\\JS-zaliczenie\\event-planner\\public\\index.html';
-
+const filePath = 'F:\\JavaScript\\event-planner\\public\\index.html';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -24,16 +22,14 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 // Połączenie z MongoDB
-mongoose.connect('mongodb://localhost:27017/event-planner', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'));
+mongoose.connect('mongodb://localhost:27017/event-planner', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // Trasy
 app.use('/events', eventRoutes);
+app.use(express.static('public'));
+app.use(express.static('src'));
 
 // Obsługa ścieżki głównej
 app.get('/', (req, res) => {
@@ -49,3 +45,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
